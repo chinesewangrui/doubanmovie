579b0323b3452423bf6dd35b0ab3643385760062
jishumahu <254600767@qq.com>
2019-10-16 20:54:52 +0800

    doubanmovie2.0

--- miniprogram/pages/charts/charts.js
+++ miniprogram/pages/charts/charts.js
@@ -5,14 +5,78 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        topMovies:[],
+        hotMovies:[],
     },
-
+    //封装函数,获取数组从大到小排列
+    objsort:function(arr,propName){
+        arr.sort((a,b)=>{
+            return b[propName] - a[propName];
+        })
+    },
+    //调用云函数获取TOP250
+    loadTop:function(){
+        wx.cloud.callFunction({
+            name:'gettop250',
+            data:{
+               start:0,
+               count:30 
+            }
+        }).then(res=>{
+            var result=JSON.parse(res.result);
+            var movies=result.subjects;
+            movies.map((elem)=>{
+                elem.average=elem.rating.average;
+                return elem
+            })
+            this.objsort(movies,"average");
+            movies=movies.slice(0,10)
+            console.log(movies)
+            this.setData({
+                topMovies:movies
+            })
+        }).catch(err=>{
+            console.log(err)
+        })
+    },
+    //调用云函数获取热映电影
+    loadHot:function(){
+        wx.cloud.callFunction({
+            name:'gethotmovies',
+            data:{
+               start:0,
+               count:30 
+            }
+        }).then(res=>{
+            var result=JSON.parse(res.result);
+            var movies=result.subjects;
+            movies.map((elem)=>{
+                elem.average=elem.rating.average;
+                return elem
+            })
+            this.objsort(movies,"average");
+            movies=movies.slice(0,10)
+            console.log(movies)
+            this.setData({
+                hotMovies:movies
+            })
+        }).catch(err=>{
+            console.log(err)
+        })
+    },
+    //前往详情页
+    goDetail:function(e){
+        var id=e.target.dataset.id;
+        wx.navigateTo({
+            url: `/pages/moviedetail/moviedetail?id=${id}`,
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.loadTop();
+        this.loadHot()
     },
 
     /**
